a5fb582360ff3118d6c5b28935e97d65a5f360e1
Bashayer7 <bashayeralbaqshi57@gmail.com>
2022-02-07 11:16:08 +0300

    done

--- apis/Recipe/controller.js
+++ apis/Recipe/controller.js
@@ -3,7 +3,7 @@ const res = require("express/lib/response");
 //to disply data (assuming that we have a data)
 exports.fetchData = async (req, res) => {
   try {
-    const data = await product.fimd({}).select("name description image");
+    const data = await product.find({}).select("name description image");
     res.json(data);
   } catch (error) {
     next(error);
