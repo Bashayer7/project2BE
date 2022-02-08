5ae1eedec2178a94701e83fd463761b9eb56d8af
Bashayer7 <bashayeralbaqshi57@gmail.com>
2022-02-08 12:12:17 +0300

    bb

--- apis/Recipe/controller.js
+++ apis/Recipe/controller.js
@@ -4,11 +4,7 @@ const recipe = require("./recipe");
 //to disply data (assuming that we have a data)
 exports.fetchData = async (req, res) => {
   try {
-<<<<<<< HEAD
     const data = await product.find({}).select("name description image");
-=======
-    const data = await recipe.find({}).select("name description image");
->>>>>>> bb
     res.json(data);
   } catch (error) {
     next(error);
