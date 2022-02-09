0388c6c5abe5691b2c2ae113d6e0dce2369bea26
Ahmed Dashti <cupld@hotmail.com>
2022-02-09 14:00:00 +0300

    remove extra details

--- apis/User/controller.js
+++ apis/User/controller.js
@@ -6,26 +6,22 @@ const passport = require ("passport");
 
 const generateToken = (newUser) =>{
     const payload ={
-        username: user.username,
-        id: user.id,
-        email: user.email,
-        firstName: user.email,
-        lastName: user.lastName,
+        username: newUser.username,
+        id: newUser.id,
+        email: newUser.email,
         exp: Date.now()+ JWT_EXP
     };
-    const token = jwt.sign(JSON.stringify(payload), JWT_SECRET);
-    res.status(201).json({message: "Successfully Created"});
-    return token;
+    return jwt.sign(JSON.stringify(payload), JWT_SECRET);
 }
 
 exports.signUp = async (req, res, next) =>{
     try{
         const saltRounds = 10;
         const hashedPassword = await bcrypt.hash(req.body.password, saltRounds);
-        req.body,password = hashedPassword;
+        req.body.password = hashedPassword;
         const user= await User.create(req.body);
-        const token = generateToken
-        json({token: token});
+        const token = generateToken(user)
+        res.json({token: token});
     }
     catch(error){
         return res.status(500).json({message: error.message});
