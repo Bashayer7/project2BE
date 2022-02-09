0af40efa504e8a6afca68c028a99b948b8877423
Ahmed Dashti <cupld@hotmail.com>
2022-02-09 13:55:01 +0300

    add cors and change route path

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
 const express = require('express');
-// const cors = require('cors');
+const cors = require('cors');
 const connectDB = require('./database');
 const categoryRoutes = require('./apis/Category/routes');
 const ingredientRoutes = require('./apis/Ingredient/routes')
-const recipeRoutes = require('./apis/Category/routes')
+const recipeRoutes = require('./apis/Recipe/routes')
 const userRoutes = require('./apis/User/routes')
 const passport = require('passport');
 const app = express();
 const { localStrategy, jwtStrtegy } = require("./middleware/passport")
 
 connectDB();
-// app.use(cors());
+app.use(cors());
 
 app.use(express.json());
 
@@ -19,9 +19,9 @@ passport.use(localStrategy);
 passport.use(jwtStrtegy)
 
 app.use("/api", userRoutes);
-app.use('/api/recipe', recipeRoutes);
-app.use('/api/category', categoryRoutes);
-app.use('./api/ingredient', ingredientRoutes);
+app.use('/api/recipes', recipeRoutes);
+app.use('/api/categories', categoryRoutes);
+app.use('/api/ingredients', ingredientRoutes);
 app.use((err, req, res, next) => {
   res.status(err.status || 500).json({
     message: err.message || 'Internal Server Error',
